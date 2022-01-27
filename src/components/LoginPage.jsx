8528a8baaa0e03185783cd4b0d48d6321b503b6b
Djordje1999 <djordjen7@gmail.com>
2022-01-27 23:11:54 +0100

    Login stranica i css

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,103 @@
+import "./css/main.css";
+import "./css/util.css";
+import { useState } from "react";
+import axios from "axios";
+
+const LoginPage = () => {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleInput(e) {
+    let newUserData = userData;
+    newUserData[e.target.name] = e.target.value;
+    setUserData(newUserData);
+  }
+
+  function handleLogin(e) {
+    e.preventDefault();
+    axios
+      .post("http://127.0.0.1:8000/api/login", userData)
+      .then((res) => {
+        console.log(res.data);
+        if(res.data.success === true){
+            window.sessionStorage.setItem("auth_token",res.data.access_token);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  return (
+    <div className="limiter">
+      <div className="container-login100">
+        <div className="wrap-login100 p-t-50 p-b-90">
+          <form
+            className="login100-form validate-form flex-sb flex-w"
+            onSubmit={handleLogin}
+          >
+            <span className="login100-form-title p-b-51">Login</span>
+
+            <div
+              className="wrap-input100 validate-input m-b-16"
+              data-validate="Email is required"
+            >
+              <input
+                className="input100"
+                type="text"
+                name="email"
+                placeholder="Email"
+                onInput={handleInput}
+              />
+              <span className="focus-input100"></span>
+            </div>
+
+            <div
+              className="wrap-input100 validate-input m-b-16"
+              data-validate="Password is required"
+            >
+              <input
+                className="input100"
+                type="password"
+                name="password"
+                placeholder="Password"
+                onInput={handleInput}
+              />
+              <span className="focus-input100"></span>
+            </div>
+
+            <div className="flex-sb-m w-full p-t-3 p-b-24">
+              <div className="contact100-form-checkbox">
+                <input
+                  className="input-checkbox100"
+                  id="ckb1"
+                  type="checkbox"
+                  name="remember-me"
+                />
+                <label className="label-checkbox100" htmlFor="ckb1">
+                  Remember me
+                </label>
+              </div>
+
+              <div>
+                <a href="#" className="txt1">
+                  Register new account here!
+                </a>
+              </div>
+            </div>
+
+            <div className="container-login100-form-btn m-t-17">
+              <button type="submit" className="login100-form-btn">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
