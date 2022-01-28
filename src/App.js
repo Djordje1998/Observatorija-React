bc5342cd1a3de58c546a684bfe0ef7586c7e1a29
Djordje1999 <djordjen7@gmail.com>
2022-01-28 08:56:26 +0100

    Dodata komponenta Stars

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginPage from './components/LoginPage';
+import logo from "./logo.svg";
+import "./App.css";
+import LoginPage from "./components/LoginPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RegisterPage from './components/RegisterPage';
+import RegisterPage from "./components/RegisterPage";
+import NavBar from "./components/NavBar";
+import { useState } from "react";
+import Stars from "./components/Stars";
 
 function App() {
+  const [token, setToken] = useState();
+
+  function addToken(auth_token) {
+    setToken(auth_token);
+  }
+
   return (
     <BrowserRouter className="App">
-    <Routes>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/register" element={<RegisterPage/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/login" element={<LoginPage addToken={addToken} />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<NavBar token={token} />}>
+          <Route href="stars" element={<Stars />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
